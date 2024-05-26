9f150e6227f8e044a493d931cf86ed501d4ddbc0
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-26 15:54:39 +0300

    urls

--- broilers/users/views.py
+++ broilers/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
 # Create your views here.
+
+from .forms import UserRegisterForm
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)  # Bind data to the form
+        if form.is_valid():
+            form.save()  # Save the form to create a new user
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username} .Log in Now!!")
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {"form": form})
