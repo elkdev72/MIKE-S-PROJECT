eb01f174d4f76ef167a2cca94998a5ff30b96bca
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-28 21:54:21 +0300

    auth app

--- broilers/users/forms.py
+++ broilers/users/forms.py
@@ -1,13 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from django.contrib.auth.forms import UserCreationForm
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    # password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
-        
